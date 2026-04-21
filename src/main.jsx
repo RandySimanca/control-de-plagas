293f33bf35617c313ac2e239a05f9e912310bce1
Randy Simanca <randysimancamercado@gmail.com>
2026-04-21 11:34:13 -0500

    creacion del archivo .npmrc para  para la solucion de conflictos con vite-plugin-pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Clean up any old service workers from previous PWA versions
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    for (const registration of registrations) {
-      registration.unregister()
-    }
-  })
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
